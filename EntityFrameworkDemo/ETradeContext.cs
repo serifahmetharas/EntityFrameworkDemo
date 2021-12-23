944daba7f80fea81915cb751d992676dcf6f58df
serifahmetharas <91914710+serifahmetharas@users.noreply.github.com>
2021-12-23 18:26:08 +0300

    Add files via upload

---
+++ EntityFrameworkDemo/ETradeContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    // Veritabanının Context classıdır.
+    class ETradeContext :DbContext
+    {
+        public DbSet<Product> Products { get; set; } // ETrade veritabanındaki tablolarda Products arıyor.
+    }
+}
